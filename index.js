52b8b423731a9b6fe8ded5d635b8cbed020021e6
jinqiwen <15256221809jin>
2018-05-20 17:48:04 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+import {AppRegistry} from 'react-native';
+import App from './App';
+import FlatListDemo from './pages/FlatListDemo';
+import SwipeableFlatListDemo from './pages/SwipeableFlatListDemo';
+import SectionListDemo from './pages/SectionListDemo';
+import {StackNavigator} from 'react-navigation'
+
+const AppRoot = StackNavigator({
+    App: {
+        screen: App,
+    },
+    FlatListDemo: {
+        screen: FlatListDemo,
+        navigationOptions: {
+            title: 'FlatListDemo'
+        }
+    },
+    SwipeableFlatListDemo: {
+        screen: SwipeableFlatListDemo,
+        navigationOptions: {
+            title: 'SwipeableFlatListDemo'
+        }
+    },
+    SectionListDemo: {
+        screen: SectionListDemo,
+        navigationOptions: {
+            title: 'SectionListDemo'
+        }
+    }
+});
+
+AppRegistry.registerComponent('FasterListDemo', () => AppRoot);
